0c117508f0e219613594e3ba0bd1fe0e3d60d2a9
Nelsonprogrammer <167528811+Nelsonprogrammer@users.noreply.github.com>
2026-06-13 13:48:58 +0200

    Change DB connection port and improve error handling
    Updated the database connection port and error message.

--- conf/db.php
+++ conf/db.php
@@ -1,17 +1,17 @@
 <?php
 
 $host = "junction.proxy.rlwy.net";
-$port = 3306;
+$port = 39852;
 $dbname = "railway";
 $user = "root";
 $password = "JHRDpWikaPlSJcyiOikFpgKpzPkacmxJ";
 
-// conexão com porta
 $conn = new mysqli($host, $user, $password, $dbname, $port);
 
-// verificar conexão
 if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+    die("Erro DB: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 
 ?>
